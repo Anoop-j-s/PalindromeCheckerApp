258b418951412a5df0b4f0c91dacc49b5aef90ab
Anoop-j-s <anoopjsbethanya@gmail.com>
2026-02-26 14:20:44 +0530

    UC1: Application Entry & Welcome Message

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(string[] args){
+        system.out.println("Welcome Palindrome Checker App");
+        system.out.println("Welcome Palindrome Checker App");
+        system.out.println("Welcome Palindrome Checker App");
+    }
+}
